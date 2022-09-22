e193a9b836d85e42165d5ed641ae5918e8fef913
Alexander <Alexyaroshovets300687@gmail.com>
2022-09-22 22:48:27 +0300

    hw-08-ready

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+const galery = galleryItems.map(({ preview, original, description }) => {
+  return `
+    <a class="gallery__item" href= ${original}>
+      <img
+        class="gallery__image"
+        src= ${preview}
+        alt=${description}
+      />
+    </a>
+  `;
+});
 
-console.log(galleryItems);
+const markup = galery.join('');
+
+const galleryEl = document.querySelector('.gallery');
+galleryEl.insertAdjacentHTML('beforeend', markup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionSelector: 'img',
+  captionType: 'alt',
+  captionPosition: 'bottom',
+  captionsData: 'alt',
+  captiondelay: 250,
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
+import { save, load, remove } from './storage';
+const formEl = document.querySelector('.feedback-form');
+const LOKALE_STORAGE_KEY = 'feedback-form-state';
 
+initPage();
+
+const handleInput = event => {
+  const { name, value } = event.target;
+
+  let saveData = load(LOKALE_STORAGE_KEY);
+  saveData = saveData ? saveData : {};
+  saveData[name] = value;
+
+  save(LOKALE_STORAGE_KEY, saveData);
+};
+formEl.addEventListener('input', throttle(handleInput, 500));
+
+function initPage() {
+  const saveData = load(LOKALE_STORAGE_KEY);
+  if (saveData) {
+    Object.entries(saveData).forEach(([name, value]) => {
+      formEl.elements[name].value = value;
+    });
+  }
+}
+const handleSubmit = event => {
+  event.preventDefault();
+  const { email, message } = event.currentTarget.elements;
+  console.log({ email: email.value, message: message.value });
+  event.currentTarget.reset();
+  remove(LOKALE_STORAGE_KEY);
+};
+formEl.addEventListener('submit', throttle(handleSubmit, 500));
