411e6ef9800f7a199b7964099f8461369517cff1
Alexander <Alexyaroshovets300687@gmail.com>
2022-09-25 11:16:26 +0300

    hw-08fixed

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,12 +3,13 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 const VIDEO_CURRENT_KEY = 'videoplayer-current-time';
+const savedTime = localStorage.getItem('videoplayer-current-time');
 player.on(
   'timeupdate',
   throttle(function (data) {
     localStorage.setItem('videoplayer-current-time', data.seconds);
   }, 1000)
 );
-if (VIDEO_CURRENT_KEY) {
-  player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+if (savedTime) {
+  player.setCurrentTime(savedTime);
 }
